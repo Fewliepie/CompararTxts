cdc509561f50489b9f25e35be802b42182c38e50
Felipe Araujo <felipearanas35@gmail.com>
2022-12-13 18:01:32 -0300

    Comparações sendo mostradas.

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ foreach (string linhas1 in texto1)
     {
     contador1++;
     }
-Console.WriteLine("{0} linhas",contador1);
-Console.WriteLine("Documento 2: ");
+    Console.WriteLine("{0} linhas",contador1);
+    Console.WriteLine("Documento 2: ");
 foreach (string linhas2 in texto2)
     {
     contador2++;
@@ -24,15 +24,22 @@ Console.WriteLine("{0} linhas", contador2);
             if (texto1[i] == texto2[i])
             {
                 Console.WriteLine("As linhas {0} são iguais.",i);
+                Console.WriteLine("");
             }
             else
             {
-                Console.WriteLine("As linhas {0} são diferentes.",i);
+                Console.WriteLine("As linhas {0} são diferentes:",i);
+                Console.WriteLine("{0} // {1}", texto1[i], texto2[i]);
+                Console.WriteLine("");
             }
+            
         }
     }                    
     else              
     {
         Console.WriteLine("Os documentos NÃO possuem a mesma quantidade de linhas.");
     }
-Console.ReadKey();
+    
+    Console.ReadKey();
+    Console.WriteLine("TECLA PRESSIONADA");
+
