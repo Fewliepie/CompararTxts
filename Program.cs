aa78e2cd28b2cbc3a135dd195d8e191fb2e9d99a
Felipe Araujo <felipearanas35@gmail.com>
2022-12-13 13:06:28 -0300

    Primeira branch

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿string[] texto1 = File.ReadAllLines(@"C:\Users\felip\Documents\DocumentosComparar\texto1.txt");
+string[] texto2 = File.ReadAllLines(@"C:\Users\felip\Documents\DocumentosComparar\texto2.txt");
+
+Console.WriteLine("Conteúdo do primeiro documento: ");
+foreach (string linhas1 in texto1)
+{
+    foreach (string linhas2 in texto2)
+    {
+    Console.WriteLine(linhas2);
+    }
+    Console.WriteLine(linhas1);
+}
+Console.WriteLine("===============================");
+Console.WriteLine("Conteúdo do segundo documento: ");
+
+//foreach(string comparar in linhas1)
+//{
+//if (linhas1 == linhas2)
+//{
+//
+//}
+
+//}
+
+Console.ReadKey();
