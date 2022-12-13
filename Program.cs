ca76c7fc39bc921dd22fb1920dc9b65d20c01d96
Felipe Araujo <felipearanas35@gmail.com>
2022-12-13 15:53:22 -0300

    Comparações funcionando

--- Program.cs
+++ Program.cs
@@ -1,16 +1,38 @@
 ﻿string[] texto1 = File.ReadAllLines(@"C:\Users\felip\Documents\DocumentosComparar\texto1.txt");
 string[] texto2 = File.ReadAllLines(@"C:\Users\felip\Documents\DocumentosComparar\texto2.txt");
+int contador1 = 0;
+int contador2 = 0;
 
-Console.WriteLine("Conteúdo do primeiro documento: ");
+Console.WriteLine("Documento 1: ");
 foreach (string linhas1 in texto1)
-{
-    
-    Console.WriteLine(linhas1);
-}
-Console.WriteLine("===============================");
-Console.WriteLine("Conteúdo do segundo documento: ");
+    {
+    contador1++;
+    }
+Console.WriteLine("{0} linhas",contador1);
+Console.WriteLine("Documento 2: ");
 foreach (string linhas2 in texto2)
     {
-    Console.WriteLine(linhas2);
+    contador2++;
+    }                            
+Console.WriteLine("{0} linhas", contador2);          
+
+    if(contador1 == contador2)
+    {
+        for( int i = 0; i < contador1; i++)
+        {
+            
+            if (texto1[i] == texto2[i])
+            {
+                Console.WriteLine("As linhas {0} são iguais.",i);
+            }
+            else
+            {
+                Console.WriteLine("As linhas {0} são diferentes.",i);
+            }
+        }
+    }                    
+    else              
+    {
+        Console.WriteLine("Os documentos NÃO possuem a mesma quantidade de linhas.");
     }
 Console.ReadKey();
